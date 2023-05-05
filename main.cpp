084fc43fc09ac37f6f6096d7c8b3a92b53b5087a
kerneyJ <gamerguy9801@gmail.com>
2023-05-04 19:38:26 -0500

    changed main

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Best time to start" << std::endl;
-    return 0;
+	std::cout << "Best time to start" << std::endl;
+	std::vector<std::vector<int>> matrix;
+	mat;
+	return 0;
 }
